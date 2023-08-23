4f2b51cdf9df84ec74179218324742f300a74168
Khembhicha <6310301032@cdti.ac.th>
2023-08-23 14:36:29 +0700

    commit

--- 1-queue.py
+++ 1-queue.py
@@ -1,3 +1,48 @@
 # we will create a producer coroutine that will generate ten random numbers 
 # and put them on the queue. We will also create a consumer coroutine 
 # that will get numbers from the queue and report their values.
+
+from random import random
+import asyncio
+import time
+
+# coroutine to generate work
+async def producer(queue):
+    print(f'{time.ctime()} Producer: Running')
+    # generate work
+    for i in range(10):
+        # generate work
+        value = random()
+        # block to simulate work
+        await asyncio.sleep(value)
+        # add to the queqe
+        await queue.put(value)
+        #print(f'{time.ctime()} Producer: put {value}')
+    # send an all done signal
+    await queue.put(None)
+    print(f'{time.ctime()} Producer: Done')
+# coroutine to consume work
+async def consumer(queue):
+    print(f'{time.ctime()} Consumer: Running')
+    # consume work
+    while True:
+        # get a unit for work
+        item = await queue.get()
+        # check for stop signal
+        if item is None:
+            break
+        # report
+        print(f'{time.ctime()} >got {item}')
+    # all done
+    print(f'{time.ctime()} Consumer: Done')
+
+# entry point coroutine
+async def main():
+    # create the shared queqe
+    queue = asyncio.Queue()
+    # start the consumer
+    _ = asyncio.create_task(consumer(queue))
+    await asyncio.gather(producer(queue), consumer(queue))
+
+# start the asyncio program
+asyncio.run(main())
